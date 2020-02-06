312c5989330369bb6db71b06cffd6f7672b6df66
shion <shion.anegasaki@gmail.com>
2020-02-06 14:44:26 +0900

    level_5

--- django_web_app/todo/models.py
+++ django_web_app/todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    text = models.CharField('text', max_length=300)
+    is_done = models.BooleanField('is done', default=False)
+
+    class Meta:
+        db_table = 'todos'
