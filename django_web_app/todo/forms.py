39ab289f26ec7335da40591ba8c982dd45855176
shion <shion.anegasaki@gmail.com>
2020-02-12 18:29:52 +0900

    update view

--- django_web_app/todo/forms.py
+++ django_web_app/todo/forms.py
@@ -5,7 +5,8 @@ class TodoForm(forms.Form):
     text = forms.CharField(
         label='やること',
         max_length=300,
-        required=True)
+        required=True,
+        )
 
     is_done = forms.ChoiceField(
       label='完了ステータス',
@@ -14,3 +15,8 @@ class TodoForm(forms.Form):
           (False, '未完'),
           (True, '完了')),
       required=True)
+
+    def __init__(self, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      for field in self.fields.values():
+          field.widget.attrs["class"] = "form-control"
