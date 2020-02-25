2c1e84b580b95a02ecdf2acb059364d06c6e23e0
shion <shion.anegasaki@gmail.com>
2020-02-26 02:34:46 +0900

    level_7

--- django_web_app/todo/views.py
+++ django_web_app/todo/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.http.response import JsonResponse
+from django.middleware.csrf import get_token
 from todo.models import Todo
 from todo.forms import TodoForm
 
@@ -22,3 +24,40 @@ def regist(request):
         todo.save()
         return redirect('/todo')
 
+def detail(request, pk):
+    form = TodoForm(request.POST)
+    todo = Todo.objects.get(pk=pk)
+    res = {}
+    if todo:
+        res = {
+            'id': pk,
+            'text': todo.text,
+            'is_done': 1 if todo.is_done else 0}
+    return JsonResponse(res)
+
+def edit(request):
+    pk = int(request.POST.get('todo_id'))
+    text = request.POST.get('text', '')
+    is_done = True if int(request.POST.get('is_done', 0)) > 0 else False
+
+    form = TodoForm({'text': text, 'is_done': is_done})
+    todo = Todo.objects.get(pk=pk)
+    form.is_valid()
+
+    if not form.is_valid() or not todo:
+        print(form.errors)
+    else:
+        todo.text=form.cleaned_data['text']
+        todo.is_done=form.cleaned_data['is_done']
+        todo.save()
+        return redirect('/todo')
+
+def delete(request):
+    pk = int(request.POST.get('todo_id'))
+    todo = Todo.objects.get(pk=pk)
+
+    if not todo:
+        print('error')
+    else:
+        todo.delete()
+        return redirect('/todo')
